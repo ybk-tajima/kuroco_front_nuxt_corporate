9fdcab8b87f42af7974207f3a4262757b50dd7a5
tajima_t <tajima_t@yamabiko-corp.co.jp>
2024-04-11 18:05:58 +0900

    modified

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,7 @@ export default defineNuxtConfig({
   ssr: false,
   runtimeConfig: {
     public: {
-      kurocoApiDomain: 'https://dev-nuxt-corporate.g.kuroco.app',
+      kurocoApiDomain: 'https://yamabiko-demo.g.kuroco.app',
     },
   },
   app: {
